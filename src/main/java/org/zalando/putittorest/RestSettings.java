2be371ece9d44d1e935a453e3a178e096217b8e8
Willi Schönborn <willi.schoenborn@zalando.de>
2017-07-21 13:13:02 +0200

    Fixed imports

--- src/main/java/org/zalando/putittorest/RestSettings.java
+++ src/main/java/org/zalando/putittorest/RestSettings.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
--- src/test/java/org/zalando/putittorest/LogbookCompatibilityTest.java
+++ src/test/java/org/zalando/putittorest/LogbookCompatibilityTest.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zalando.logbook.spring.LogbookAutoConfiguration;
-import org.zalando.logbook.spring.LogbookHttpClientAutoConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -19,7 +18,6 @@ public final class LogbookCompatibilityTest {
     @Import(DefaultTestConfiguration.class)
     @ImportAutoConfiguration({
             LogbookAutoConfiguration.class,
-            LogbookHttpClientAutoConfiguration.class,
             JacksonAutoConfiguration.class
     })
     public static class TestConfiguration {
